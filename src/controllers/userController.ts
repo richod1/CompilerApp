5a82167bf7a2ec8d3d7a946ce9b4cc3cd9642266
richod1 <frimpsup@gmail.com>
2024-04-09 00:42:32 +0000

    signUp func added

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -31,7 +31,48 @@ export const userDetails=async(req:AuthRequest,res:Response)=>{
  * signup function @signup
  */
 
-export const signUp=async()=>{
+export const signUp=async(req:AuthRequest,res:Response)=>{
+    const {email,password,username}=req.body;
+    const usernameRegex=/^[a-zA-z0-9]+$/;
+    try{
+        const existingUser=await User.findOne({email:email})
+        if(existingUser){
+            return res.status(400).send({message:"User already exist"})
+        }
+        if(!usernameRegex.test(username)){
+            return res.status(400).send({message:"Username is not aligned!"})
+        }
+
+        /**
+         * salting user and hash passowd @jwt
+         */
+        const genSaltRound:number=10;
+        const salt=await bcrypt.genSalt(genSaltRound);
+        const hashedPassword=await bcrypt.hash(password,salt);
+        const user=await User.create({
+            email:email,
+            password:hashedPassword,
+            username:username,
+        })
+
+        const jwtToken=jwt.sign({_id:user._id,email:user.email},process.env.JWT_KEY!,{expiresIn:"2d"});
+        res.cookie("token",jwtToken,{
+            path:"/",
+            expires:new Date(Date.now() * 1000 *60*60*48),
+            httpOnly:true,
+            sameSite:"lax"
+        })
+
+        return res.status(201).send({
+            username:user.username,
+            picture:user.picture,
+            email:user.email,
+            savedCodes:user.savedCodes,
+        })
+    }catch(error){
+        return res.status(500).send({message:"Having problem signing!"})
+
+    }
 
 }
 
